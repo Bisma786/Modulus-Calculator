af9b1676a77c9075c31543a05910a2bb024a5439
Bisma Gondal <159830145+Bisma786@users.noreply.github.com>
2024-03-05 00:29:43 +0500

    Add files via upload

---
+++ mod.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+const valueOne = await inquirer.prompt({
+    name: "val1",
+    type: "number",
+    message: "Enter the number of obejcts"
+});
+const ValueTwo = await inquirer.prompt({
+    name: "val2",
+    type: "number",
+    message: "Enter the number of containers",
+});
+let total = valueOne.val1 % ValueTwo.val2;
+console.log(total);
